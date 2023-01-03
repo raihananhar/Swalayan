09a68649c978963a7509d6e29032e1bc65413974
Yudhapermanaa <203040101@mail.unpas.ac.id>
2023-01-03 16:59:57 +0700

    memperbaiki halaman home dan menambahkan halaman cart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Slide;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,10 +14,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
@@ -23,6 +26,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = Product::latest()->get();
+        $categories = Category::whereNull('category_id')->take(4)->get();
+        $slides = Slide::latest()->get();
+
+        return view('frontend.homepage', compact('products', 'categories', 'slides'));
     }
 }
